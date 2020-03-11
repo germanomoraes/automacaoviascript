aea4cbe5ea8034287a8b3c1901d5cde0370daeba
aluno <aluno@Lab-15>
2020-03-11 14:25:52 -0300

    Alterações do programa germano

--- converterimagens.sh
+++ converterimagens.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+#Trabalho do germano  and vicente
 unzip imagens-livros.zip
+
 cd imagens-livros
+
 convert *.jpg teste.png
+
 mkdir novapasta 
+
 mv *.png imagens-livros.zip
+
 zip -r imagens-livros.png.zip imagens-livros.png/
 
 
